23c2a624fb0c8b92f9b1674928ffdfd4fbe44771
popi753 <luka.popi.919@gmail.com>
2023-07-05 17:00:09 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+// import { useEffect, useState } from 'react';
+
+
+
+axios.get('http://localhost:5000/users')
+.then(response => {
+  console.log(response.data);
+}).catch((error) => {
+  console.log(error)
+});
+
+
+
+
 
 function App() {
+  // const [data, setdata] = useState([]);
+
+ 
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/users')
+  //     .then(response => {
+  //       setdata(response.data);
+  //     });
+  // } 
+  // , []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* {data.map((item) => {
+          return <div key={item._id}>{item.username}</div>
+        })} */}
+
     </div>
   );
 }
